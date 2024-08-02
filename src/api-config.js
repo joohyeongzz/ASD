07bab82e838994aa85eaeb78775a90787ca4bc0c
Kang, Jooheong <joohyeongzz@naver.com>
2024-08-02 07:18:51 +0000

    asd

--- src/api-config.js
+++ src/api-config.js
@@ -3,9 +3,9 @@ let backendHost; //백엔드 호스트 주소 결정
 const hostname = window && window.location && window.location.hostname;
 
 if(hostname === "localhost") {
-  backendHost = "http://localhost:8080";
+  backendHost = "https://www.awskjh.p-e.kr";
 } else {
-    backendHost = "http://localhost:8080";
+    backendHost = "https://www.awskjh.p-e.kr";
 }
 
 export const API_BASE_URL = `${backendHost}`;
