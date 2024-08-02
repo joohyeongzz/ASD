7cb4e8c0a50a52b9115f1197d38c9c20508f40a9
Kang, Jooheong <joohyeongzz@naver.com>
2024-08-02 06:45:03 +0000

    asd

--- src/api-config.js
+++ src/api-config.js
@@ -4,7 +4,7 @@ const hostname = window && window.location && window.location.hostname;
 
 if(hostname === "localhost") {
   backendHost = "https://todo-backend-dev2.ap-northeast-2.elasticbeanstalk.com";
-} else {s
+} else {
     backendHost = "https://todo-backend-dev2.ap-northeast-2.elasticbeanstalk.com";
 }
 
