832b4c7f00c47e40d0dfd87f1a0b94c7228bc007
Kang, Jooheong <joohyeongzz@naver.com>
2024-08-02 06:38:50 +0000

    Update api-config.js

--- src/api-config.js
+++ src/api-config.js
@@ -3,9 +3,9 @@ let backendHost; //백엔드 호스트 주소 결정
 const hostname = window && window.location && window.location.hostname;
 
 if(hostname === "localhost") {
-  backendHost = "http://192.168.100.108:8080";
+  backendHost = "http://todo-backend-dev2.ap-northeast-2.elasticbeanstalk.com";
 } else {
-    backendHost = "http://192.168.100.108:8080";
+    backendHost = "http://todo-backend-dev2.ap-northeast-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
