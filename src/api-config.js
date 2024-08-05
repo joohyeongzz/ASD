320d1a0dc392aa4088440354b1029582d453de44
Kang, Jooheong <joohyeongzz@naver.com>
2024-08-05 07:54:56 +0000

    asd

--- src/api-config.js
+++ src/api-config.js
@@ -6,7 +6,7 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === "localhost") {
   backendHost = "http://localhost:8080"; // 로컬 개발 서버 주소
 } else {
-  backendHost = "http://backendtest-env.eba-3tevwkbf.ap-northeast-2.elasticbeanstalk.com"; // 배포된 서버 주소
+  backendHost = "https://www.awskjh.p-e.kr/"; // 배포된 서버 주소
 }
 
 export const API_BASE_URL = backendHost;
